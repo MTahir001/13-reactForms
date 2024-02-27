a2a85f6fcbdba897ddaa9308df5092787885a07f
MTahir01 <tahirkadgaonkar01@gmail.com>
2024-02-27 09:47:19 +0530

    handling error msg of invalid fields on lost of focus

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header.jsx";
 import SignUp from "./components/SignUp.jsx";
+import Login from "./components/Login.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <main>
+        <Login />
         <SignUp />
       </main>
     </>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: {
+      value: "",
+      didEdit: false,
+    },
+    password: {
+      value: "",
+      didEdit: false,
+    },
+  });
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prev) => {
+      return {
+        ...prev,
+        [identifier]: { ...prev[identifier], value, didEdit: false },
+      };
+    });
+  }
+  function handleInputValidation(identifier) {
+    setEnteredValues((prev) => {
+      return {
+        ...prev,
+        [identifier]: { ...prev[identifier], didEdit: true },
+      };
+    });
+    console.log(`lost focus from ${identifier}`);
+  }
+
+  const emailIsInValid =
+    enteredValues.email.didEdit && !enteredValues.email.value.includes("@");
+
+  console.log(emailIsInValid);
   return (
     <form>
       <h2>Login</h2>
@@ -6,12 +41,29 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(e) => handleInputChange("email", e.target.value)}
+            value={enteredValues.email.value}
+            onBlur={() => handleInputValidation("email")}
+          />
+          <div className="control-error">
+            {emailIsInValid && <p>Please enter valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(e) => handleInputChange("password", e.target.value)}
+            value={enteredValues.password.value}
+            onBlur={() => handleInputValidation("password")}
+          />
         </div>
       </div>
 
